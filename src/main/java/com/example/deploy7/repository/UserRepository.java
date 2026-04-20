ddd38f8b491f584622598be54cb420bd0a1f3139
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2026-04-20 11:16:45 +0700

    mengisi code UserRepository

---
+++ src/main/java/com/example/deploy7/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.deploy7.repository;
+
+import com.example.deploy7.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
